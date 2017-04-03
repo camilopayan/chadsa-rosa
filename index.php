876f8fc159dbe31d803f29be5e77c6e2a55df4a0
Camilo Payan <camilo@camilopayan.com>
2017-04-03 16:04:38 -0400

    Initial commit
    This takes a design from HTML5 Up, brings in some new graphics, many
    changes to the Sass, and of course, new content.
    
    Also, I'm going to be adding some more PHP to this in the future, so
    I've changed index.html to index.php.

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE HTML>
+<!--
+	Solid State by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Chattanooga Democratic Socialists of America</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="dist/scripts/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="dist/styles/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="dist/styles/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="dist/styles/ie8.css" /><![endif]-->
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/manifest.json">
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+		<meta name="theme-color" content="#ffffff">
+		<meta property="og:title" content="Chattanooga Democratic Socialists of America">
+		<meta property="og:url" content="https://dsachattanooga.org/">
+		<meta property="og:type" content="website">
+		<meta property="og:description" content="Join the movement for socialist equality and justice in Chattanooga, Tennessee! Fight for change and people's power.">
+		<meta property="og:image" content="https://dsachattanooga.org/dist/images/dsa-logo.jpg">
+		<meta property="twitter:title" content="Chattanooga Democratic Socialists of America">
+		<meta property="twitter:description" content="Join the movement for socialist equality and justice in Chattanooga, Tennessee! Fight for change and people's power.">
+		<meta property="twitter:image" content="https://dsachattanooga.org/dist/images/dsa-logo.jpg">
+		<meta property="twitter:url" content="https://dsachattanooga.org/">
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">DSA Chattanooga</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="#what-is-dsa">What is DSA?</a></li>
+								<li><a href="#why-chatt">Why Chattanooga?</a></li>
+								<li><a href="#upcoming-events">Upcoming Events</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"><img src="dist/images/dsa-logo.jpg" /></div>
+							<h2>Chattanooga<br />Democratic Socialists of America</h2> 
+							<div class="social-media">
+								<a href="https://twitter.com/dsa_chatt"><span class="fa fa-twitter fa-large"></span></a>
+								<a href="https://facebook.com/ChattanoogaDemocraticSocialistsofAmerica/"><span class="fa fa-facebook fa-large"></span></a>
+							</div>
+							<p>Learn more by subscribing:</p>
+
+							<form action="//facebook.us15.list-manage.com/subscribe/post?u=30e8b4a961e3a2a2ecf92c181&amp;id=70c3b63a7d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+								<div id="mc_embed_signup_scroll">
+
+									<div class="mc-field-group field">
+										<label for="mce-EMAIL">Email</label>
+										<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+									</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+									<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_30e8b4a961e3a2a2ecf92c181_70c3b63a7d" tabindex="-1" value=""></div>
+									<ul class="actions">
+										<li><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></li>
+									</ul>
+								</div>
+							</form>
+						</div>
+					</section>
+
+				<!-- Wrapper -->
+					<section id="wrapper">
+
+						<!-- One -->
+							<section id="what-is-dsa" class="wrapper spotlight style1">
+								<div class="inner">
+									<a href="#" class="image"><img src="dist/images/pic01.jpg" alt="" /></a>
+									<div class="content">
+										<h2 class="major">What is DSA?</h2>
+										<p>The Democratic Socialists of America (DSA) is the largest socialist organization in the United States. DSA's members are building progressive movements for social change while establishing an openly democratic socialist presence in American communities and politics. DSA fights for reforms today that will weaken the power of corporations and increase the power of working people.</p>
+										<a href="http://www.dsausa.org/" target="_blank">Learn more about DSA</a>
+									</div>
+								</div>
+							</section>
+
+						<!-- Two -->
+							<section id="why-chatt" class="wrapper alt spotlight style2">
+								<div class="inner">
+									<a href="#" class="image"><img src="dist/images/pic02.jpg" alt="" /></a>
+									<div class="content">
+										<h2 class="major">Why Chattanooga?</h2>
+										<p>Our members are Chattanoogans who believe that democratic socialism will gain support in Chattanooga. We want to connect and support the fights for equality and justice already underway in Chattanooga. We want to add our own perspective where it's welcome.</p>
+										<p>Chattanooga is growing rapidly. It's up to us to make sure that the growth does not become a nightmare of inequality. By demanding a say in Chattanooga's destiny, we ensure that this growth leaves no one behind.</p>
+									</div>
+								</div>
+							</section>
+
+						<!-- Four -->
+							<section id="upcoming-events" class="wrapper alt style1">
+								<div class="inner">
+									<h2 class="major">Meet DSA</h2>
+									<p>DSA members are already supporting existing struggles, organizing new ones, and educating themselves. Here are upcoming events DSA members will be attending: </p>
+									<section class="features">
+										<article>
+											<a href="#" class="image"><img src="dist/images/vigil.jpeg" alt="" /></a>
+											<h3 class="major">Vigil for the Affordable Care Act</h3>
+											<p>Wednesday March 22, 7:30 pm at the Walnut Street Bridge.</p>
+										</article>
+										<article>
+											<a href="https://www.facebook.com/events/252458611870192/" class="image"><img src="dist/images/grownooga.jpg" alt="" /></a>
+											<h3 class="major">Grownooga 2017</h3>
+											<p>Grownooga is a community event to help the activist community in Chattanooga connect.</p>
+											<a href="https://www.facebook.com/events/252458611870192/" class="special">Learn more</a>
+										</article>
+										<article>
+											<a href="https://www.facebook.com/events/588366488034072/" class="image"><img src="dist/images/pic05.jpg" alt="" /></a>
+											<h3 class="major">Fem Jam Poetry Slam</h3>
+											<p>Join the Intersectional Feminist Alliance for a night of poetry and express yourself.</p>
+											<a href="https://www.facebook.com/events/588366488034072/" class="special">Learn more</a>
+										</article>
+										<article>
+											<a href="https://www.meetup.com/Chattanooga-Democratic-Socialists-of-America/events/238565167/" class="image"><img src="dist/images/jacobin.png" alt="" /></a>
+											<h3 class="major">Jacobin Reading Group</h3>
+											<p>We take some time to read and discuss articles from Jacobin magazine.</p>
+											<a href="https://www.meetup.com/Chattanooga-Democratic-Socialists-of-America/events/238565167/" class="special">Learn more</a>
+										</article>
+									</section>
+								</div>
+							</section>
+
+					</section>
+
+				<!-- Footer -->
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Keep up with DSA</h2>
+							<form action="//facebook.us15.list-manage.com/subscribe/post?u=30e8b4a961e3a2a2ecf92c181&amp;id=70c3b63a7d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+								<div id="mc_embed_signup_scroll">
+
+									<div class="mc-field-group field">
+										<label for="mce-EMAIL">Email</label>
+										<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+									</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+									<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_30e8b4a961e3a2a2ecf92c181_70c3b63a7d" tabindex="-1" value=""></div>
+									<ul class="actions">
+										<li><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></li>
+									</ul>
+								</div>
+							</form>
+							<ul class="contact">
+								<li class="fa-twitter"><a href="https://twitter.com/dsa_chatt">DSA Chattanooga on Twitter</a></li>
+								<li class="fa-facebook"><a href="https://facebook.com/ChattanoogaDemocraticSocialistsofAmerica/">DSA Chattanooga on Facebook</a></li>
+							</ul>
+							<ul class="copyright">
+								<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="dist/scripts/skel.min.js"></script>
+			<script src="dist/scripts/main.js"></script>
+			<script src="dist/scripts/main.js"></script>
+			<script src="dist/scripts/main.js"></script>
+
+	</body>
+</html>
